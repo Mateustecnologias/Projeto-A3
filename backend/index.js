1b55ff88ba5a0f52ae9ee1f65aad16711d957f6c
piloote03 <muriloschineider1107@gmail.com>
2024-05-29 17:35:41 -0300

    primeiro commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); // Configurando o Dotenv e pegando o valor da variável de ambiente
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+
+const express = require('express'); // configuração do express //
+const app = express();
+
+app.use(express.json()); // o app irá usar o as requisições http em dados compartilhados em formato de JSON //
+const PORT = 4000
+app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`)) // serviço irá rodar na porta 4000 //
+
+const cors = require("cors"); // configuração do cors (é um mecanismo utilizado pelos navegadores para compartilhar recursos entre diferentes origens.) //
+app.use(cors());
+app.options('*', cors()); // dizemos para que o cors seja utilizado para requisições de todos os endereços //
+
+const { OpenAI } = require("openai");
+const openai = new OpenAI(OPENAI_API_KEY); // criamos uma instância do objeto da api validando com a chave passada no .env
+
+app.post('/chatbox', async (req, res) => { // metodo POST que possibilita receber uma resposta do que foi enviado pela req //
+    const completion = await openai.chat.completions.create({
+        messages:
+            [{
+                role: "user",
+                content: req.body.prompt
+            }],
+        model: "gpt-3.5-turbo", // modelo que será usado na consulta a API // 
+        max_tokens: 500, // define a quantidade de caracters a resposta ira consumir //
+        temperature: 0.9 // define o grau de aleatoriedade da resposta //
+    });
+
+    //console.log(completion); // conseguimos ver como é composta a estrutura da resposta do ChatGPT // 
+    res.send(response.data.choices[0].message.content); // caminho na qual a resposta vem do console //
+});
